0849dc7943294bc63fb70a57219aade186e3de91
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-04-09 10:30:48 +0300

    change name

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow container mx-auto px-4 py-8">
-        <h2 className="text-3xl font-bold mb-6">New Movies</h2>
+        <h2 className="text-3xl font-bold mb-6">Featured Movies</h2>
         <MovieList />
       </main>
     </div>
