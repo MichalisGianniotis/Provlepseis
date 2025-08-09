4140e4fb14d4643f8fdaa7ebfee25461764c3ee0
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-08-09 12:28:35 +0300

    9/8 commit

--- components/common/CTA.jsx
+++ components/common/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
     return (
         <section className="mt-12 bg-muted p-8 rounded-lg text-center">
             <h2 className="text-2xl font-bold mb-4">🔒 Θες Πρόσβαση στις VIP Προβλέψεις;</h2>
-            <p className="mb-4">📲 Στείλε μας τώρα μήνυμα στο Instagram με τη λέξη "<b>VIP</b>"
+            <p className="mb-4">📲 Στείλε μας τώρα μήνυμα στο Instagram με τη λέξη &quot;<b>VIP</b>&quot;
             και μάθε πώς θα αποκτήσεις καθημερινές προβλέψεις με <b>έως 60% περισσότερες πιθανότητες επαλήθευσης.</b></p>
             {/* <button>Subscribe Now</button> */}
         </section>
